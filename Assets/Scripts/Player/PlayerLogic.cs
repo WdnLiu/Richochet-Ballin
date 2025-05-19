54cbd40d6c9447618df006270c1420f90d8369fe
Yuma Ishigooka de la Calle <114680656+YumaIshigooka@users.noreply.github.com>
2025-05-19 16:29:05 +0200

    player feedback (#11)
    * pues lo de la rama
    
    * disable collisions of bullet when dissapearing

--- Assets/Scripts/Player/PlayerLogic.cs
+++ Assets/Scripts/Player/PlayerLogic.cs
@@ -5,22 +5,21 @@ using UnityEngine;
 public class PlayerLogic : MonoBehaviour
 {
     public GameObject[] elementsSetMaterial;
-    // Start is called before the first frame update
 
+    // Start is called before the first frame update
 
     private void setMaterials()
     {
         for (int i = 0; i < elementsSetMaterial.Length; i++)
-            elementsSetMaterial[i].GetComponent<Renderer>().material = GetComponent<Renderer>().material;
+            elementsSetMaterial[i].GetComponent<Renderer>().material =
+                GetComponent<Renderer>().material;
     }
+
     void Start()
     {
         setMaterials();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 }
